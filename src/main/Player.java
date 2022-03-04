7162e8441a0e1d40bbc1a617463bd24a5caaef05
sashabondar41 <sashabondar41@gmial.com>
2022-03-04 21:14:47 +0300

    Create class Player and test for Player

---
+++ src/main/Player.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Player {
+    private String name;
+
+    public Player() {
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/tests/PlayerTest.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import main.Player;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+    @Test
+    public void testName(){
+        String name = "Захар";
+        Player first = new Player();
+        String result = first.getName();
+        assertEquals(name, result);
+    }
+}
