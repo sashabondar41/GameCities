78182545f00f95dd185c0ee5b18847ee681db0c8
sashabondar41 <sashabondar41@gmial.com>
2022-03-04 22:16:48 +0300

    Change public modifyCity method to private

--- src/main/Cities.java
+++ src/main/Cities.java
@@ -11,7 +11,7 @@ import java.util.Locale;
 
 public class Cities {
     private static final String url = "https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D0%B3%D0%BE%D1%80%D0%BE%D0%B4%D0%BE%D0%B2_%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D0%B8";
-    public static String modifyCity(String city){
+    private static String modifyCity(String city){
         StringBuilder result = new StringBuilder(city.toLowerCase(Locale.ROOT).replace('ё', 'е'));
         if (result.indexOf("(") != -1) {
             result.delete(city.indexOf("("), city.indexOf(")") + 1);
