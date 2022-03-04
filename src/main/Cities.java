df292612148d9111168ef2aea1c83f19edd6f570
sashabondar41 <sashabondar41@gmial.com>
2022-03-04 21:46:36 +0300

    Create class Cities and test for Cities

---
+++ src/main/Cities.java
@@ -0,0 +1,14 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cities {
+    public static String modifyCity(String city){
+        return city;
+    }
+
+    public static List<String> getCities(){
+        return new ArrayList<>();
+    }
+}
